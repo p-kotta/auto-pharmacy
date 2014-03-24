5c9e3ab8f674e70ea131be35a9ba5261eacfc525
Sergey Berezhnoy <veged@yandex-team.ru>
2014-03-25 03:22:47 +0400

    /admin/works/: edit existing

--- main.py
+++ main.py
@@ -9,37 +9,56 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+WORK_TEMPLATE = """\
+    <h2 id="%s">%s</h2>
+    <p>%s</p>
+"""
 class WorksHandler(webapp2.RequestHandler):
     def get(self):
         for work in Work.query().order(+Work.priority).fetch():
-            self.response.write('<h2 id="%s">%s</h2>' % (work.name, work.title))
-            self.response.write('<p>%s</p>' % work.description)
+            self.response.write(WORK_TEMPLATE % (work.name, work.title, work.description))
 
 class AdminHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         self.response.write('Hello, ' + user.nickname())
 
-WORKS_ADMIN_TEMPLATE = """\
+WORK_ADMIN_TEMPLATE = """\
     <form action="" method="post">
-        <input name="priority" value="42"/>
-        <input name="name" value="name"/>
-        <input name="title" value="Title"/>
-        <textarea name="description">Description</textarea>
+        <input name="key" type="hidden" value="%s"/>
+        <input name="name" value="%s"/>
+        <input name="title" value="%s"/>
+        <textarea name="description">%s</textarea>
+        <input name="priority" value="%s"/>
         <input type="submit" value="Submit"/>
     </form>
 """
 class WorksAdminHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(WORKS_ADMIN_TEMPLATE)
+        # edit existing
+        for work in Work.query().order(+Work.priority).fetch():
+            self.response.write(WORK_ADMIN_TEMPLATE % (work.key.urlsafe(), work.name, work.title, work.description, work.priority))
+
+        # add new
+        self.response.write(WORK_ADMIN_TEMPLATE % ('', '', '', '', ''))
 
     def post(self):
-        work = Work()
+        # try get existing...
+        work = None
+        try:
+            work = ndb.Key(urlsafe=self.request.get('key')).get()
+        except TypeError:
+            pass
+        # or create new one
+        if not work:
+            work = Work()
+
         work.priority = int(self.request.get('priority'))
         work.name = self.request.get('name')
         work.title = self.request.get('title')
         work.description = self.request.get('description')
         work.put()
+
         self.redirect('/works/')
 
 app = webapp2.WSGIApplication([
