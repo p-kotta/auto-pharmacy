68933f816b941d05ca78428d3c7eeb4780c974c8
Sergey Berezhnoy <veged@yandex-team.ru>
2014-03-25 00:32:53 +0400

    admin sector stub

--- main.py
+++ main.py
@@ -15,11 +15,24 @@
 # limitations under the License.
 #
 import webapp2
+from google.appengine.api import users
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class AdminHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        if user:
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/admin/', AdminHandler)
 ], debug=True)
