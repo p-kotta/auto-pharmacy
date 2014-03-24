1e33efdf6644da5eae277b242207a2bfe1411adc
Sergey Berezhnoy <veged@yandex-team.ru>
2014-03-25 01:59:52 +0400

    works stub

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class WorksHandler(webapp2.RequestHandler):
+    def get(self):
+        for work in Work.query().order(+Work.priority).fetch():
+            self.response.write('<h2 id="%s">%s</h2>' % (work.name, work.title))
+            self.response.write('<p>%s</p>' % work.description)
+
 class AdminHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        self.response.write('Hello, ' + user.nickname())
 
-        if user:
-            self.response.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+WORKS_ADMIN_TEMPLATE = """\
+    <form action="" method="post">
+        <input name="priority" value="42"/>
+        <input name="name" value="name"/>
+        <input name="title" value="Title"/>
+        <textarea name="description">Description</textarea>
+        <input type="submit" value="Submit"/>
+    </form>
+"""
+class WorksAdminHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(WORKS_ADMIN_TEMPLATE)
 
+    def post(self):
+        work = Work()
+        work.priority = int(self.request.get('priority'))
+        work.name = self.request.get('name')
+        work.title = self.request.get('title')
+        work.description = self.request.get('description')
+        work.put()
+        self.redirect('/works/')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/admin/', AdminHandler)
+    ('/works/', WorksHandler),
+
+    ('/admin/', AdminHandler),
+    ('/admin/works/', WorksAdminHandler)
 ], debug=True)
+
+
+class Work(ndb.Model):
+    priority = ndb.IntegerProperty()
+    name = ndb.StringProperty()
+    title = ndb.StringProperty()
+    description = ndb.StringProperty()
