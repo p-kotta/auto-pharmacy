ccc83dbdc6b7441006096d9ed7a34421592adfcd
Yulia <yusokolova@yandex-team.ru>
2014-04-30 04:05:42 +0400

    change styles

--- handlers/works.py
+++ handlers/works.py
@@ -6,13 +6,7 @@ from templates import render
 
 class WorksHandler(webapp2.RequestHandler):
     def get(self):
-
-        self.response.write(render('head', {}))
-
-        self.response.write(render('header', { 'current': 'works' }))
-
         self.response.write(render(
             'works',
             { 'works': Work.get_root()['children'] }))
 
-        self.response.write(render('footer', {}))
